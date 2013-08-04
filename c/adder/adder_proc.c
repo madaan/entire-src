6add173a450ce67243043d5bf494f1d4f2f4653c
Aman Madaan <aman@aman-VPCEB14EN.(none)>
2013-08-05 00:28:26 +0530

    More mod

--- c/adder/adder_proc.c
+++ c/adder/adder_proc.c
@@ -7,19 +7,20 @@
 
 char **adder_1(struct operation **opn, CLIENT *req)
 {
-    printf("Solving!");
-  
     mpz_t n11, n12, n13;
     mpz_init(n11);
     mpz_init(n12);
     mpz_init(n13);
     mpz_set_str(n11, (*opn)->op1, 10);
     mpz_set_str(n12, (*opn)->op2, 10);
+    FILE *f = fopen("/home/aman/console", "w");
+    fprintf(f, "%s, %s, %c\n", (*opn)->op1, (*opn)->op2, (*opn)->operator);
     switch((*opn)->operator) {
+      
         case '+':
         mpz_add(n13, n11, n12);
         break;
-        
+
         case '-':
         mpz_sub(n13, n11, n12);
         break;
@@ -33,16 +34,13 @@ char **adder_1(struct operation **opn, CLIENT *req)
         mpz_cdiv_q(n13, n11, n12);
         break;
     }
-    
-    FILE *f;
- 
-	f = fopen("/home/aman/console", "w");
+
 	if (f == (FILE *)NULL) {
 		return NULL;
 	}
+
 	fprintf(f, "%s\n", "dfdf");
 	fclose(f);
-    
     char *str = malloc(1000);
     mpz_get_str(str, 10, n13);
     static char *result;
