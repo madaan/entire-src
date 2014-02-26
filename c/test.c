2d3fdad5cde162584fea5904be9ca049c3930088
Aman Madaan <madaan.amanmadaan@gmail.com>
2014-02-26 20:52:05 +0530

    add twonorm cross validation code

--- c/test.c
+++ c/test.c
@@ -1,100 +1,24 @@
-#include<stdio.h>
-/*
-int main()
-{
-int a = (1,2,3);
-int b = (1,2);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-if(a == b)
-printf("Equal");
-else
-printf("Not Equal");
-return 0;
-}
+int stack_pointer = 0;
+char *stack[30]; //MAX NUMBER OF VALUES IN STACK IS 30
 
-void foo(char *);
-int main()
-{
-char *string = "Hello";
-foo(string);
-printf("%s",string);
-return 0;
-}
-void foo(char *a)
-{
-while(*a)
-{
-*a += 1;
-printf("\n%c",*a);
-a++;
-}
-}
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-char s[] = "Opendays2012";
-int i = 0;
-int a[]={1,2,3};
-a++;
-}
-/*(a) Segmentation Fault
-(b) Compile Error
-(c) 12
-(d) 0
+int main(int argc, char** argv) {
 
-#include<stdio.h>
-int a = 10;
-int main()
-{
-fun();
-fun();
-return 0;
-}
+    char command_line[256];
 
-int fun()
-{
-static int a = 1;
-printf("%d ",a);
-a++;
-return 0;
-}
+    while(stack_pointer < 5) {  //Just store 5 inputs
+        stack[stack_pointer] = malloc(sizeof(char) * 256);
+        printf("repl> ");
+        scanf("%s",command_line);
+        strcpy(stack[stack_pointer], command_line);  
+        stack_pointer++;
+    }   
 
-#include<stdio.h>
-int main()
-{
-int a[10][20][30] = {0};
-a[5][2][1] = 2;
-
-return 0;
+    int i = 0;
+    for(i = 0; i<5; i++) {
+        printf("stack[%d] =  %s\n", i, stack[i]);
+    }
 }
-(a) printf("%d",*(((a+5)+2)+1));
-(b) printf("%d",***((a+5)+2)+1);
-(c) printf("%d",*(*(*(a+5)+2)+1));
-(d) None of these
-
-#include<stdio.h>
-#define x 4+1
-int main()
-{
-int i;
-i = x*x*x;
-printf("%d",i);
-return 0;
-}
-
-#include<stdio.h>
-int main()
-{
-if(!(printf("Hello ")))
-{
-printf("Hello ");
-}
-else
-{
-printf("World\n");
-}
-return 0;
-}
-*/
-
---
+++ py/twonorm/svm.py
@@ -0,0 +1,25 @@
+#sg
+import numpy as np
+import pylab as pl
+from sklearn.svm import SVC
+from sklearn.datasets import load_iris
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.grid_search import GridSearchCV
+
+def cvalidate():
+    from sklearn import cross_validation
+
+    trainset = np.genfromtxt(open('Twonorm','r'), delimiter=' ')
+    X = np.array([x[0:20] for x in trainset])
+    y = np.array([x[20] for x in trainset])
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.3, random_state = 0)
+    C_range = 10.0 ** np.arange(-2, 9)
+    gamma_range = 10.0 ** np.arange(-5, 4)
+    param_grid = dict(gamma=gamma_range, C=C_range)
+    grid = grid_search.GridSearchCV(SVC(), param_grid=param_grid)
+    grid.fit(X_train, y_train)
+    print(grid.best_estimator_)
+if __name__ == '__main__':
+    cvalidate()
+
+    
